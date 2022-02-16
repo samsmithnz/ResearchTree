994570cd8528cf4da7624eaf02b043c66c108aed
Sam Smith <samsmithnz@gmail.com>
2022-02-16 16:44:12 -0500

    Resized for

--- src/ResearchTree.WinForms/frmMain.Designer.cs
+++ src/ResearchTree.WinForms/frmMain.Designer.cs
@@ -145,14 +145,14 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(998, 477);
+            this.comboBox1.Location = new System.Drawing.Point(998, 670);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(374, 40);
             this.comboBox1.TabIndex = 11;
             // 
             // btnHideButtons
             // 
-            this.btnHideButtons.Location = new System.Drawing.Point(1222, 416);
+            this.btnHideButtons.Location = new System.Drawing.Point(1222, 618);
             this.btnHideButtons.Name = "btnHideButtons";
             this.btnHideButtons.Size = new System.Drawing.Size(150, 46);
             this.btnHideButtons.TabIndex = 12;
@@ -164,7 +164,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1384, 529);
+            this.ClientSize = new System.Drawing.Size(1384, 722);
             this.Controls.Add(this.btnHideButtons);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label3);
