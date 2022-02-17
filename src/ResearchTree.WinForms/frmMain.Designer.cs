5f800e8043e4a69c2a8e61bb67a5d598bd734e45
Sam Smith <samsmithnz@gmail.com>
2022-02-16 22:33:51 -0500

    Removed old placeholders

--- src/ResearchTree.WinForms/frmMain.Designer.cs
+++ src/ResearchTree.WinForms/frmMain.Designer.cs
@@ -28,119 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.btnHideButtons = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(50, 200);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(162, 100);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Visible = false;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(293, 50);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(162, 100);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Visible = false;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(293, 350);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(162, 100);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Visible = false;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(536, 200);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(162, 100);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Visible = false;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(779, 200);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(162, 100);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Visible = false;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(1022, 200);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(162, 100);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Visible = false;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(779, 50);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(162, 100);
-            this.button7.TabIndex = 7;
-            this.button7.Text = "button7";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Visible = false;
-            // 
-            // label1
-            // 
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Location = new System.Drawing.Point(215, 250);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 2);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "label1";
-            this.label1.Visible = false;
-            // 
-            // label2
-            // 
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Location = new System.Drawing.Point(248, 100);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(2, 150);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "label2";
-            this.label2.Visible = false;
-            // 
-            // label3
-            // 
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Location = new System.Drawing.Point(250, 100);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 2);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "label3";
-            this.label3.Visible = false;
             // 
             // comboBox1
             // 
@@ -167,16 +57,6 @@
             this.ClientSize = new System.Drawing.Size(1384, 722);
             this.Controls.Add(this.btnHideButtons);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "frmMain";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -184,17 +64,6 @@
         }
 
         #endregion
-
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button6;
-        private Button button5;
-        private Button button7;
-        private Label label1;
-        private Label label2;
-        private Label label3;
         private ComboBox comboBox1;
         private Button btnHideButtons;
     }
