32288bb8d0bf2a66a9bbb793f2b63f0011161ac3
Sam Smith <samsmithnz@gmail.com>
2022-02-20 10:17:28 -0500

    Added basic cubes

--- ResearchTree/Assets/MainLoop.cs
+++ ResearchTree/Assets/MainLoop.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ResearchTree;
+using ResearchTree.Models;
 
 public class MainLoop : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        List<ResearchItem> items = ResearchPool.BuildDemoList();
+        ResearchController controller = new ResearchController(items,
+            16, 10,
+            8, 5);
+
+        //Draw the nodes
+        foreach (ResearchItem item in items)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.name = item.Name;
+            cube.transform.position = new Vector3(item.Location.X, item.Location.Y, item.Location.Z);
+            cube.transform.localScale = new Vector3(controller.ItemWidth, controller.ItemHeight, 1);
+
+            //Button button = new();
+            //button.Text = item.Name;
+            //button.Location = new Point((int)item.Location.X, (int)item.Location.Y);
+            //button.Width = item.Width;
+            //button.Height = item.Height;
+            //this.Controls.Add(button);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
